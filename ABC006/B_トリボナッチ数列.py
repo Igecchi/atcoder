991451f06b624c5adbdbfad89903e46601d1afea
Igecchi <h.igetta07@gmail.com>
2021-07-02 23:58:12 +0900

    Do ABC006 B

---
+++ ABC006/B_トリボナッチ数列.py
@@ -0,0 +1,16 @@
+N = int(input())
+a1 = 0
+a2 = 0
+a3 = 1
+a_list = [0, 0, 1]
+
+
+if N <= 2:
+    print(0)
+elif N == 3:
+    print(1)
+else:
+    for i in range(3, N):
+        a_list.append(a_list[i-1] + a_list[i-2] + a_list[i-3])
+
+print(a_list[-1]%10007)
