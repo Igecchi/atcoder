96afb62473f5f65fc8ec546a8349e0117622935b
Igecchi <h.igetta07@gmail.com>
2021-08-04 23:30:41 +0900

    Do ABC006 B

--- ABC006/B_トリボナッチ数列.py
+++ ABC006/B_トリボナッチ数列.py
@@ -14,5 +14,14 @@ else:
         a_list.append(a_list[i-1] + a_list[i-2] + a_list[i-3])
 
 print(a_list[-1]%10007)
-
 #やり直す必要あり↑
+
+## 再度挑戦↓
+*a, = [0, 0, 1]
+n = int(input())
+if n <= 3:
+    print(a[n-1])
+else:
+    for i in range(3, n):
+        a.append((a[i-1] + a[i-2] + a[i-3]) % 10007)
+    print(a[-1] % 10007)
