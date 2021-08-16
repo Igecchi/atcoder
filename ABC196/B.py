ccf47c37647a66a343bdcae290be823c43c28688
Igecchi <h.igetta07@gmail.com>
2021-08-16 23:53:23 +0900

    modify ABC196 B& make ABC213

--- ABC196/B.py
+++ ABC196/B.py
@@ -1,5 +1,11 @@
-import math
-from decimal import *
-X = input()
-x = math.floor(Decimal(X))
+# import math
+# from decimal import *
+# X = input()
+# x = math.floor(Decimal(X))
+# print(x)
+
+X = input().split(".")
+# print(X)
+
+x = X[0]
 print(x)
