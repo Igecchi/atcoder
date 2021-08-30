542f69d1ed58b9afa0b63971e74fe97c09212c4c
Igecchi <h.igetta07@gmail.com>
2021-08-31 01:15:57 +0900

    Update B.py
    Add sample answer

--- ABC083/B.py
+++ ABC083/B.py
@@ -9,3 +9,19 @@ for i in range(1, N+1):
     if A <= tmp_sum <= B:
         ans += i
 print(ans)
+
+# tmp_sum = sum(map(int, list(str(i)))) #こっちの方がスマート！
+
+#参考回答（https://atcoder.jp/contests/abc083/submissions/1899403）
+#10ごとにリセットする
+# n, a, b = (int(x) for x in input().split())
+# c = 0
+# sum = 0
+# for i in range(1, n+1):
+#   c += 1
+#   if a <= c <= b:
+#     sum += i
+#   while i%10 == 9:
+#     c -= 9
+#     i //= 10
+# print(sum)
