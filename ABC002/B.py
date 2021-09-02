7b6fa520fc278ce3711329e5dea48becb903add2
Igecchi <h.igetta07@gmail.com>
2021-09-02 23:30:31 +0900

    Update B.py
    Add other answers

--- ABC002/B.py
+++ ABC002/B.py
@@ -1,7 +1,12 @@
 W = input()
-W = W.replace("a", "")
-W = W.replace("i", "")
-W = W.replace("u", "")
-W = W.replace("e", "")
-W = W.replace("o", "")
+# W = W.replace("a", "")
+# W = W.replace("i", "")
+# W = W.replace("u", "")
+# W = W.replace("e", "")
+# W = W.replace("o", "")
+# print(W)
+
+boin = ("a", "i", "u", "e", "o")
+for x in boin:
+    W = W.replace(x, "")
 print(W)
