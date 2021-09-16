66505969fa6cfda2cea2ed805f71154095f7f56e
Igecchi <h.igetta07@gmail.com>
2021-09-16 11:26:01 +0900

    Modify code

--- ABC216/B.py
+++ ABC216/B.py
@@ -2,10 +2,10 @@ import sys
 N = int(input())
 N_list = [input().split() for _ in range(N)]
 
-for i in range(3):
+for i in range(N):
     s = N_list[i][0]
     t = N_list[i][1]
-    for j in range(i+1, 3):
+    for j in range(i+1, N):
         if N_list[j][0] == s:
             if N_list[j][1] == t:
                 print("Yes")
