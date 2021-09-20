f1df2408bbc285b6bbba6bc2221da6f64e0e74c6
Igecchi <h.igetta07@gmail.com>
2021-09-20 23:10:47 +0900

    Update C.py

--- ABC219/C.py
+++ ABC219/C.py
@@ -1,3 +1,4 @@
+## My answer
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 X = input()
 N = int(input())
@@ -13,3 +14,16 @@ for _ in range(N):
 # dict_sorted = sorted(ans_dict.items(), key=lambda x:x[0])
 # print(dict_sorted)
 for y in sorted(ans_dict.items(), key=lambda x:x[0]): print(y[1])
+
+
+## Other answer
+X, N, *S = open(0).read().split()
+# print(X)
+# print(S)
+# print(dict(zip(X, range(26))))
+table = str.maketrans(dict(zip(X, range(26))))
+# print(table)
+S.sort(key=lambda s: s.translate(table))
+
+for s in S:
+    print(s)
